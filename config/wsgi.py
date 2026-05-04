a60b240c62904738b486530b480e31572c5cd14d
Joel Lucio <joel.bizerra16@gmail.com>
2026-05-04 12:50:46 -0300

    fix: criação correta de superuser customizado

--- config/wsgi.py
+++ config/wsgi.py
@@ -16,6 +16,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.prod')
 application = get_wsgi_application()
 
 from django.contrib.auth import get_user_model
+from django.db import transaction
 
 
 def create_admin_safe():
@@ -23,13 +24,27 @@ def create_admin_safe():
 		User = get_user_model()
 		username = "admin2"
 		password = "123456"
+		has_email_field = any(field.name == 'email' for field in User._meta.fields)
 
 		if not User.objects.filter(username=username).exists():
-			User.objects.create_superuser(
-				username=username,
-				email="admin@wms.com",
-				password=password
-			)
+			with transaction.atomic():
+				create_kwargs = {
+					'username': username,
+					'nome': 'admin2',
+					'perfil': 'GESTOR',
+					'setor': 'NAO_ENCONTRADO',
+					'is_staff': True,
+					'is_superuser': True,
+					'is_active': True,
+				}
+				if has_email_field:
+					create_kwargs['email'] = 'admin2'
+
+				user = User(**create_kwargs)
+				user.set_password(password)
+				user.save()
+				if hasattr(user, 'definir_setores'):
+					user.definir_setores(['NAO_ENCONTRADO'])
 			print("ADMIN CRIADO COM SUCESSO")
 		else:
 			print("ADMIN JÁ EXISTE")
